7575122d37fe8f218a26070f462cdfcb6ee8376a
unalCe <uunal.celik@gmail.com>
2019-07-16 18:07:14 +0300

    Initial Commit

---
+++ LoodosQuest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoodosQuest
+//
+//  Created by Unal Celik on 16.07.2019.
+//  Copyright © 2019 unalCelik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
